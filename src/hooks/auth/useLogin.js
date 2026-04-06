de45a65a8e924f3ab70c5826a8d6a932f0f1c365
MhmdAkbar <akbar.muhammad.offc@gmail.com>
2026-04-06 19:09:33 +0700

    trigger redeploy

--- src/hooks/auth/useLogin.js
+++ src/hooks/auth/useLogin.js
@@ -1,54 +1,63 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
-import { api } from '@/utils/api';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import { api } from "@/utils/api";
 
 export const useLogin = () => {
   const { t } = useTranslation();
   const navigate = useNavigate();
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
 
   const handleLogin = async (e) => {
     e.preventDefault();
-    setLoading(true); 
-    setError('');
+    setLoading(true);
+    setError("");
 
     try {
-      const result = await api('/api/auth/login', {
-        method: 'POST',
-        body: JSON.stringify({ email, password })
+      const result = await api("/api/auth/login", {
+        method: "POST",
+        body: JSON.stringify({ email, password }),
       });
-      
+
       if (result.success) {
         const token = result.data.token;
         const user = result.data.user;
-
+        console.log("ENV:", import.meta.env.VITE_API_BASE_URL);
         if (token) {
-          localStorage.setItem('token', token);
-          localStorage.setItem('userEmail', user.email);
-          localStorage.setItem('userName', user.name); 
-          localStorage.setItem('userRole', user.role);
-          
-          navigate('/dashboard');
+          localStorage.setItem("token", token);
+          localStorage.setItem("userEmail", user.email);
+          localStorage.setItem("userName", user.name);
+          localStorage.setItem("userRole", user.role);
+
+          navigate("/dashboard");
         } else {
-          throw new Error('Login berhasil, tapi token tidak ditemukan di JSON.');
+          throw new Error(
+            "Login berhasil, tapi token tidak ditemukan di JSON.",
+          );
         }
       }
     } catch (err) {
-      setError(err.message === 'Failed to fetch' ? 'Koneksi ke Server Gagal (Backend Mati/CORS).' : err.message);
+      setError(
+        err.message === "Failed to fetch"
+          ? "Koneksi ke Server Gagal (Backend Mati/CORS)."
+          : err.message,
+      );
     } finally {
       setLoading(false);
     }
   };
 
   return {
-    email, setEmail,
-    password, setPassword,
-    loading, error,
-    handleLogin
+    email,
+    setEmail,
+    password,
+    setPassword,
+    loading,
+    error,
+    handleLogin,
   };
 };
